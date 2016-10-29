c056a4212c6c9cbcd7f9e03de14057f97eeba467
preich <preich@192.168.1.101>
2016-10-29 18:38:35 +0100

    microlife calculation

---
+++ microlife.py
@@ -0,0 +1,26 @@
+# based on http://journals.bmj.com/site/microlives/main.js
+class MicrolifeCalculator:
+
+  def __init__(self):
+    self.is_male = False
+
+  def gender(self,x):
+    if x == "male":
+      self.is_male=True
+      return -4
+    return 0
+
+  def smoking(self,x):
+     return (-10 if self.is_male else -9) if x > 15 else 0
+
+  def alcohol(self,x):
+    if x < 2:
+      return x
+    # TODO: really? you can drink 1130 units and it's the same damage as 5 units?
+    return 1.0 - min(x-1,5) * (0.5 if self.is_male else 1)
+
+  
+calc = MicrolifeCalculator()
+print(calc.gender('male'))
+print(calc.smoking(17))
+print(calc.alcohol(1130))
